fbaa2563a75dfab5b06c6d395bed3ea23054fd8d
Sebas <seplarui@gmail.com>
2019-10-30 20:53:10 +0100

    add jumbotron component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './componentes/nav/nav.component';
+import { JumbotronComponent } from './componentes/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
